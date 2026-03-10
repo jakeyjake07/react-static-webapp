f2d5d5db26960a355f962fc8d4ae7508088a2d33
jakeyjake07 <jaakob@live.se>
2026-03-10 12:11:00 +0100

    update UI

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -9,6 +9,9 @@ function App() {
   return (
     <>
       <div>
+
+        <h1>Hello from Azure Static Web Apps</h1>
+
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
